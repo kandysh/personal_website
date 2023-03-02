5242770a4e1a7fe809ca70f4b17841637bafdcc7
kandysh <rajawat.k.029@gmail.com>
2023-03-02 13:35:18 +0530

    congifg

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // // https://vitejs.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
 // })
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true,
-    },
-    host: true, // needed for the Docker Container port mapping to work
-    strictPort: true,
-    port: 5173, // you can replace this port with any port
-  }})
+	plugins: [react()],
+	server: {
+		watch: {
+			usePolling: true,
+		},
+		https: true,
+		hmr: {
+			host: 'me.kandysh.xyz',
+			port: 5174,
+			protocol: 'wss',
+		},
+		host: true, // needed for the Docker Container port mapping to work
+		strictPort: true,
+		port: 5173, // you can replace this port with any port
+	},
+});
