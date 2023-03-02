8b64eed9dcc18385a26e12dddbe2ad9ed61eedfc
kandysh <rajawat.k.029@gmail.com>
2023-03-02 14:03:40 +0530

    viteconfig

--- vite.config.js
+++ vite.config.js
@@ -11,12 +11,12 @@ export default defineConfig({
 		watch: {
 			usePolling: true,
 		},
-		https: true,
-		hmr: {
-			host: 'me.kandysh.xyz',
-			port: 5174,
-			protocol: 'wss',
-		},
+		// https: true,
+		// hmr: {
+		// 	host: 'me.kandysh.xyz',
+		// 	port: 5174,
+		// 	protocol: 'wss',
+		// },
 		host: true, // needed for the Docker Container port mapping to work
 		strictPort: true,
 		port: 5173, // you can replace this port with any port
